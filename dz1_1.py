bebf3458f203aaa6daf5a08734750591d4a6786e
Polina <polya.toropina@gmail.com>
2024-02-02 19:57:26 +0200

    add dz 1_1

---
+++ dz1_1.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import shape
+
+shape('turtle')
+turtle.pensize(100000)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+turtle.color('red', 'yellow')
+turtle.pendown()
+turtle.left(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(150)
