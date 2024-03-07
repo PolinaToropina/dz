c152e6082f755a2369e2cce53cc86f257dce1343
Polina <polya.toropina@gmail.com>
2024-03-07 16:06:22 +0200

    dz 10

--- dz10.py
+++ dz10.py
@@ -1,17 +1,35 @@
-group_names = ["Поліна", "Ірина", "Всеволод"]
+import tkinter as tk
+from tkinter import messagebox
 
-def check_login(name):
-    if name in group_names:
-        print("Ви успішно увійшли!")
+group_names = ["Торопіна Поліна Всеволодівна", "Сіренко Ірина Дмітрівна", "Торопін Всеволод Миколайович"]
+
+def check_login():
+    user_name = entry.get()
+    if user_name in group_names:
+        messagebox.showinfo("Успішний вхід", "Ви успішно увійшли!")
+        info = entry_info.get("1.0", "end-1c")
+        with open("user_info.txt", "w") as file:
+            file.write(info)
     else:
-        print("Вас немає в базі даних. Що вам треба?")
+        messagebox.showwarning("Помилка", "Вас немає в базі даних. Що вам треба?")
 
+root = tk.Tk()
+root.title("Вхід")
+root.geometry("300x200")
 
-    if name in group_names:
-        input("Інформація:")
-        print("Інформація збережена")
+label = tk.Label(root, text="Введіть ваше ім'я:")
+label.pack()
 
+entry = tk.Entry(root)
+entry.pack()
 
-user_name = input("Введіть ваше ім'я: ")
+label_info = tk.Label(root, text="Введіть інформацію.Якщо можливо англійськіми літерами):")
+label_info.pack()
 
-check_login(user_name)
+entry_info = tk.Text(root, height=4)
+entry_info.pack()
+
+button = tk.Button(root, text="Увійти", command=check_login)
+button.pack()
+
+root.mainloop()
